3e1aaa45d22dc82e780f532d781af7cf022a4c85
jiasm <jiashunming@outlook.com>
2016-05-04 14:41:48 +0800

    使用多个canvas

--- dist/krakout.js
+++ dist/krakout.js
@@ -21,32 +21,56 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     _classCallCheck(this, Krakout);
 
     var self = this;
+    var canvasList = self._canvas = {}; // 存放画布的集合 球 板 背景 各种...
+
+    // 初始化游戏
     function init() {
       // 创建基础的画布
-      var canvas = document.createElement('canvas');
-      // 设置画布的宽高
-      setScreen(container, canvas);
-      // 将canvas添加至容器
-      container.appendChild(canvas);
-
-      var ctx = self.context = canvas.getContext('2d');
-      ctx.font = "48px serif";
-      ctx.fillText("Hello world", 10, 50);
+      buildCanvas(['ball', 'board', 'layout'], buildScreen(container));
     }
 
-    function setScreen(container, canvas) {
-      canvas.width = width;
-      canvas.height = height;
-      canvas.style.width = global.getComputedStyle(container).width;
-      canvas.style.height = global.getComputedStyle(container).height;
+    // 生成画布
+    function buildCanvas(tag, callback) {
+      if (typeof tag !== 'string') {
+        for (var key in tag) {
+          buildCanvas(tag[key], callback);
+        }
+      } else {
+        // 创建画布
+        var canvas = document.createElement('canvas');
+
+        // 设置画布定位方式 用于重叠显示
+        canvas.style.position = 'absolute';
+        canvas.style.top = 0;
+        canvas.style.left = 0;
+
+        // 设置唯一标识
+        canvas.id = canvas.className = 'krakout-canvas-' + tag;
+
+        // 将canvas添加至容器
+        container.appendChild(canvas);
+
+        // 设置画布的宽高
+        callback(canvas);
+
+        // 获取canvas的context 并存入canvasList
+        canvasList[tag] = canvas.getContext('2d');
+      }
     }
 
+    // 设置画布的宽高
+    function buildScreen(container) {
+      return function (canvas) {
+        canvas.width = width;
+        canvas.height = height;
+        canvas.style.width = global.getComputedStyle(container).width;
+        canvas.style.height = global.getComputedStyle(container).height;
+      };
+    }
+
+    // 调用初始化方法
     init();
-  }
-
-  // 静态方法
-
-  ;
+  };
 
   return Krakout;
 }, window);
